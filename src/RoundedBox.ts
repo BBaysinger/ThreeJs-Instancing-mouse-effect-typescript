5390b20c8685d786cbd346adc08f4508c1ce9720
Bradley Baysinger <bhbaysinger@gmail.com>
2025-02-16 23:56:10 -0800

    Converted RoundedBox to TS.

--- src/RoundedBox.ts
+++ src/RoundedBox.ts
@@ -1,15 +1,29 @@
-import * as THREE from "three";
+import { BufferGeometry, BufferAttribute, Vector3 } from "three";
 
 /**
  * @author pailhead / http://dusanbosnjak.com
  * @author benolayinka / github.com/benolayinka
  */
 
-class RoundedBoxGeometry extends THREE.BufferGeometry {
-  constructor(width, height, depth, radius, radiusSegments) {
+class RoundedBoxGeometry extends BufferGeometry {
+  parameters: {
+    width: number;
+    height: number;
+    depth: number;
+    radius: number;
+    radiusSegments: number;
+  };
+
+  constructor(
+    width: number = 1,
+    height: number = 1,
+    depth: number = 1,
+    radius: number = 0.15,
+    radiusSegments: number = 1,
+  ) {
     super();
 
-    this.type = "RoundedBoxGeometry";
+    // this.type = "RoundedBoxGeometry";
 
     //validate params ===================================
 
@@ -28,49 +42,50 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
       Math.min(width, Math.min(height, Math.min(depth))) / 2,
     );
 
-    var edgeHalfWidth = width / 2 - radius;
-    var edgeHalfHeight = height / 2 - radius;
-    var edgeHalfDepth = depth / 2 - radius;
+    const edgeHalfWidth = width / 2 - radius;
+    const edgeHalfHeight = height / 2 - radius;
+    const edgeHalfDepth = depth / 2 - radius;
 
     //not sure why this is needed, for querying? ========
 
     this.parameters = {
-      width: width,
-      height: height,
-      depth: depth,
-      radius: radius,
-      radiusSegments: radiusSegments,
+      width,
+      height,
+      depth,
+      radius,
+      radiusSegments,
     };
 
     //calculate vertices count ==========================
 
-    var rs1 = radiusSegments + 1; //radius segments + 1
+    const rs1 = radiusSegments + 1; //radius segments + 1
 
-    var totalVertexCount = (rs1 * radiusSegments + 1) << 3;
+    const totalVertexCount = (rs1 * radiusSegments + 1) << 3;
 
     //make buffers ======================================
 
-    var positions = new THREE.BufferAttribute(
+    const positions = new BufferAttribute(
       new Float32Array(totalVertexCount * 3),
       3,
     );
 
-    var normals = new THREE.BufferAttribute(
+    const normals = new BufferAttribute(
       new Float32Array(totalVertexCount * 3),
       3,
     );
 
     //some vars =========================================
 
-    var cornerVerts = [],
-      cornerNormals = [],
-      // normal = new THREE.Vector3(),
-      vertex = new THREE.Vector3(),
-      vertexPool = [],
-      normalPool = [],
-      indices = [];
-    var lastVertex = rs1 * radiusSegments,
+    const cornerVerts: Vector3[][] = [],
+      cornerNormals: Vector3[][] = [],
+      vertex = new Vector3(),
+      vertexPool: Vector3[] = [],
+      normalPool: Vector3[] = [],
+      indices: number[] = [];
+
+    const lastVertex = rs1 * radiusSegments,
       cornerVertNumber = rs1 * radiusSegments + 1;
+
     doVertices();
     doFaces();
     doCorners();
@@ -80,54 +95,54 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
 
     // calculate vert positions =========================
 
-    function doVertices() {
+    function doVertices(): void {
       //corner offsets
-      var cornerLayout = [
-        new THREE.Vector3(1, 1, 1),
-        new THREE.Vector3(1, 1, -1),
-        new THREE.Vector3(-1, 1, -1),
-        new THREE.Vector3(-1, 1, 1),
-        new THREE.Vector3(1, -1, 1),
-        new THREE.Vector3(1, -1, -1),
-        new THREE.Vector3(-1, -1, -1),
-        new THREE.Vector3(-1, -1, 1),
+      const cornerLayout: Vector3[] = [
+        new Vector3(1, 1, 1),
+        new Vector3(1, 1, -1),
+        new Vector3(-1, 1, -1),
+        new Vector3(-1, 1, 1),
+        new Vector3(1, -1, 1),
+        new Vector3(1, -1, -1),
+        new Vector3(-1, -1, -1),
+        new Vector3(-1, -1, 1),
       ];
 
       //corner holder
-      for (var j = 0; j < 8; j++) {
+      for (let j = 0; j < 8; j++) {
         cornerVerts.push([]);
         cornerNormals.push([]);
       }
 
       //construct 1/8 sphere ==============================
 
-      var PIhalf = Math.PI / 2;
+      const PIhalf = Math.PI / 2;
 
-      var cornerOffset = new THREE.Vector3(
+      const cornerOffset = new Vector3(
         edgeHalfWidth,
         edgeHalfHeight,
         edgeHalfDepth,
       );
 
-      for (var y = 0; y <= radiusSegments; y++) {
-        var v = y / radiusSegments;
+      for (let y = 0; y <= radiusSegments; y++) {
+        const v = y / radiusSegments;
 
-        var va = v * PIhalf; //arrange in 90 deg
+        const va = v * PIhalf; //arrange in 90 deg
 
-        var cosVa = Math.cos(va); //scale of vertical angle
+        const cosVa = Math.cos(va); //scale of vertical angle
 
-        var sinVa = Math.sin(va);
+        const sinVa = Math.sin(va);
 
         if (y == radiusSegments) {
           vertex.set(0, 1, 0);
 
-          var vert = vertex.clone().multiplyScalar(radius).add(cornerOffset);
+          const vert = vertex.clone().multiplyScalar(radius).add(cornerOffset);
 
           cornerVerts[0].push(vert);
 
           vertexPool.push(vert);
 
-          var norm = vertex.clone();
+          const norm = vertex.clone();
 
           cornerNormals[0].push(norm);
 
@@ -136,10 +151,10 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
           continue; //skip row loop
         }
 
-        for (var x = 0; x <= radiusSegments; x++) {
-          var u = x / radiusSegments;
+        for (let x = 0; x <= radiusSegments; x++) {
+          const u = x / radiusSegments;
 
-          var ha = u * PIhalf;
+          const ha = u * PIhalf;
 
           //make 1/8 sphere points
           vertex.x = cosVa * Math.cos(ha);
@@ -147,7 +162,7 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
           vertex.z = cosVa * Math.sin(ha);
 
           //copy sphere point, scale by radius, offset by half whd
-          var vert = vertex.clone().multiplyScalar(radius).add(cornerOffset);
+          const vert = vertex.clone().multiplyScalar(radius).add(cornerOffset);
 
           cornerVerts[0].push(vert);
 
@@ -155,7 +170,7 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
 
           //sphere already normalized, just clone
 
-          var norm = vertex.clone().normalize();
+          const norm = vertex.clone().normalize();
           cornerNormals[0].push(norm);
           normalPool.push(norm);
         }
@@ -163,15 +178,15 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
 
       //distribute corner verts ===========================
 
-      for (var i = 1; i < 8; i++) {
-        for (var j = 0; j < cornerVerts[0].length; j++) {
-          var vert = cornerVerts[0][j].clone().multiply(cornerLayout[i]);
+      for (let i = 1; i < 8; i++) {
+        for (let j = 0; j < cornerVerts[0].length; j++) {
+          const vert = cornerVerts[0][j].clone().multiply(cornerLayout[i]);
 
           cornerVerts[i].push(vert);
 
           vertexPool.push(vert);
 
-          var norm = cornerNormals[0][j].clone().multiply(cornerLayout[i]);
+          const norm = cornerNormals[0][j].clone().multiply(cornerLayout[i]);
 
           cornerNormals[i].push(norm);
 
@@ -183,25 +198,25 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
     // weave corners ====================================
 
     function doCorners() {
-      // var indexInd = 0;
+      // const indexInd = 0;
 
-      var flips = [true, false, true, false, false, true, false, true];
+      const flips = [true, false, true, false, false, true, false, true];
 
-      var lastRowOffset = rs1 * (radiusSegments - 1);
+      const lastRowOffset = rs1 * (radiusSegments - 1);
 
-      for (var i = 0; i < 8; i++) {
-        var cornerOffset = cornerVertNumber * i;
+      for (let i = 0; i < 8; i++) {
+        const cornerOffset = cornerVertNumber * i;
 
-        for (var v = 0; v < radiusSegments - 1; v++) {
-          var r1 = v * rs1; //row offset
-          var r2 = (v + 1) * rs1; //next row
+        for (let v = 0; v < radiusSegments - 1; v++) {
+          const r1 = v * rs1; //row offset
+          const r2 = (v + 1) * rs1; //next row
 
-          for (var u = 0; u < radiusSegments; u++) {
-            var u1 = u + 1;
-            var a = cornerOffset + r1 + u;
-            var b = cornerOffset + r1 + u1;
-            var c = cornerOffset + r2 + u;
-            var d = cornerOffset + r2 + u1;
+          for (let u = 0; u < radiusSegments; u++) {
+            const u1 = u + 1;
+            const a = cornerOffset + r1 + u;
+            const b = cornerOffset + r1 + u1;
+            const c = cornerOffset + r2 + u;
+            const d = cornerOffset + r2 + u1;
 
             if (!flips[i]) {
               indices.push(a);
@@ -223,10 +238,10 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
           }
         }
 
-        for (var u = 0; u < radiusSegments; u++) {
-          var a = cornerOffset + lastRowOffset + u;
-          var b = cornerOffset + lastRowOffset + u + 1;
-          var c = cornerOffset + lastVertex;
+        for (let u = 0; u < radiusSegments; u++) {
+          const a = cornerOffset + lastRowOffset + u;
+          const b = cornerOffset + lastRowOffset + u + 1;
+          const c = cornerOffset + lastVertex;
 
           if (!flips[i]) {
             indices.push(a);
@@ -246,10 +261,10 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
 
     function doFaces() {
       //top
-      var a = lastVertex; // + cornerVertNumber * 0;
-      var b = lastVertex + cornerVertNumber; // * 1;
-      var c = lastVertex + cornerVertNumber * 2;
-      var d = lastVertex + cornerVertNumber * 3;
+      let a = lastVertex; // + cornerVertNumber * 0;
+      let b = lastVertex + cornerVertNumber; // * 1;
+      let c = lastVertex + cornerVertNumber * 2;
+      let d = lastVertex + cornerVertNumber * 3;
 
       indices.push(a);
       indices.push(b);
@@ -327,16 +342,16 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
     // weave edges ======================================
 
     function doHeightEdges() {
-      for (var i = 0; i < 4; i++) {
-        var cOffset = i * cornerVertNumber;
-        var cRowOffset = 4 * cornerVertNumber + cOffset;
-        var needsFlip = i & (1 === 1);
-        for (var u = 0; u < radiusSegments; u++) {
-          var u1 = u + 1;
-          var a = cOffset + u;
-          var b = cOffset + u1;
-          var c = cRowOffset + u;
-          var d = cRowOffset + u1;
+      for (let i = 0; i < 4; i++) {
+        const cOffset = i * cornerVertNumber;
+        const cRowOffset = 4 * cornerVertNumber + cOffset;
+        const needsFlip = i & 1;
+        for (let u = 0; u < radiusSegments; u++) {
+          const u1 = u + 1;
+          const a = cOffset + u;
+          const b = cOffset + u1;
+          const c = cRowOffset + u;
+          const d = cRowOffset + u1;
 
           if (!needsFlip) {
             indices.push(a);
@@ -358,23 +373,23 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
     }
 
     function doDepthEdges() {
-      var cStarts = [0, 2, 4, 6];
-      var cEnds = [1, 3, 5, 7];
+      const cStarts = [0, 2, 4, 6];
+      const cEnds = [1, 3, 5, 7];
 
-      for (var i = 0; i < 4; i++) {
-        var cStart = cornerVertNumber * cStarts[i];
-        var cEnd = cornerVertNumber * cEnds[i];
+      for (let i = 0; i < 4; i++) {
+        const cStart = cornerVertNumber * cStarts[i];
+        const cEnd = cornerVertNumber * cEnds[i];
 
-        var needsFlip = 1 >= i;
+        const needsFlip = 1 >= i;
 
-        for (var u = 0; u < radiusSegments; u++) {
-          var urs1 = u * rs1;
-          var u1rs1 = (u + 1) * rs1;
+        for (let u = 0; u < radiusSegments; u++) {
+          const urs1 = u * rs1;
+          const u1rs1 = (u + 1) * rs1;
 
-          var a = cStart + urs1;
-          var b = cStart + u1rs1;
-          var c = cEnd + urs1;
-          var d = cEnd + u1rs1;
+          const a = cStart + urs1;
+          const b = cStart + u1rs1;
+          const c = cEnd + urs1;
+          const d = cEnd + u1rs1;
 
           if (needsFlip) {
             indices.push(a);
@@ -396,26 +411,26 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
     }
 
     function doWidthEdges() {
-      var end = radiusSegments - 1;
+      const end = radiusSegments - 1;
 
-      var cStarts = [0, 1, 4, 5];
-      var cEnds = [3, 2, 7, 6];
-      var needsFlip = [0, 1, 1, 0];
+      const cStarts = [0, 1, 4, 5];
+      const cEnds = [3, 2, 7, 6];
+      const needsFlip = [0, 1, 1, 0];
 
-      for (var i = 0; i < 4; i++) {
-        var cStart = cStarts[i] * cornerVertNumber;
-        var cEnd = cEnds[i] * cornerVertNumber;
+      for (let i = 0; i < 4; i++) {
+        const cStart = cStarts[i] * cornerVertNumber;
+        const cEnd = cEnds[i] * cornerVertNumber;
 
-        for (var u = 0; u <= end; u++) {
-          // var dInd = u != end ? radiusSegments + u * rs1 : cornerVertNumber - 1;
+        for (let u = 0; u <= end; u++) {
+          // const dInd = u != end ? radiusSegments + u * rs1 : cornerVertNumber - 1;
 
-          var a = cStart + radiusSegments + u * rs1;
-          var b =
+          const a = cStart + radiusSegments + u * rs1;
+          const b =
             cStart +
             (u != end ? radiusSegments + (u + 1) * rs1 : cornerVertNumber - 1);
 
-          var c = cEnd + radiusSegments + u * rs1;
-          var d =
+          const c = cEnd + radiusSegments + u * rs1;
+          const d =
             cEnd +
             (u != end ? radiusSegments + (u + 1) * rs1 : cornerVertNumber - 1);
 
@@ -440,9 +455,9 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
 
     //fill buffers ======================================
 
-    var index = 0;
+    let index = 0;
 
-    for (var i = 0; i < vertexPool.length; i++) {
+    for (let i = 0; i < vertexPool.length; i++) {
       positions.setXYZ(
         index,
         vertexPool[i].x,
@@ -455,7 +470,7 @@ class RoundedBoxGeometry extends THREE.BufferGeometry {
       index++;
     }
 
-    this.setIndex(new THREE.BufferAttribute(new Uint16Array(indices), 1));
+    this.setIndex(new BufferAttribute(new Uint16Array(indices), 1));
 
     this.setAttribute("position", positions);
 
